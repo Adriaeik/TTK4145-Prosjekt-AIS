501696f656884d6b7d9d68102baee6987b0fdee7
ravnbudde <ravnravnerik@gmail.com>
2025-02-26 00:16:25 +0100

    funker?

--- elevator_pro/src/main.rs
+++ elevator_pro/src/main.rs
@@ -22,11 +22,11 @@ async fn main() {
 
 
 /*Skaper oss eit verdensbildet ved fødselen, vi tar vår første pust */
-    let worldview_serialised = init::initialize_worldview();
+    let mut worldview_serialised = init::initialize_worldview();
     
 /* START ----------- Init av lokale channels ---------------------- */
     //Kun bruk mpsc-rxene fra main_local_chs
-    let main_local_chs = local_network::LocalChannels::new();
+    let mut main_local_chs = local_network::LocalChannels::new();
     let _ = main_local_chs.watches.txs.wv.send(worldview_serialised.clone());
 /* SLUTT ----------- Init av lokale channels ---------------------- */
 
@@ -44,10 +44,10 @@ async fn main() {
 /* SLUTT ----------- Kloning av lokale channels til Tokio Tasks ---------------------- */                                                     
 
 
-    let _update_wv_task = tokio::spawn(async move {
-        utils::print_info("Starter å oppdatere wv".to_string());
-        let _ = world_view_ch::update_wv(main_local_chs, worldview_serialised).await;
-    });
+    // let _update_wv_task = tokio::spawn(async move {
+    //     utils::print_info("Starter å oppdatere wv".to_string());
+    //     let _ = world_view_ch::update_wv(main_local_chs, worldview_serialised).await;
+    // });
     //TODO: Få den til å signalisere at vi er i known state. Den kommer ikke til å returnere etterhvert
     // let _local_elev_task = tokio::spawn(async {
     //     let _ = tcp_self_elevator::run_local_elevator(chs_local_elev).await;
@@ -91,12 +91,111 @@ async fn main() {
         }
     });
 
-    loop {
-        sleep(Duration::from_millis(100)).await;
+
+
+    println!("Starter update_wv");
+    let _ = main_local_chs.watches.txs.wv.send(worldview_serialised.clone());
+    let mut wv_des = world_view::deserialize_worldview(&worldview_serialised);
+    let init_task = world_view::Task{
+        id: u16::MAX,
+        to_do: 0,
+    };
+    if let Some(i) = world_view::get_index_to_container(utils::SELF_ID.load(Ordering::SeqCst), worldview_serialised) {
+        wv_des.elevator_containers[i].tasks.push(init_task); //Antar at vi er eneste heisen i systemet mikromillisekundet vi starter
     }
+    
+    worldview_serialised = world_view::serialize_worldview(&wv_des);
+    let _ = main_local_chs.watches.txs.wv.send(worldview_serialised.clone());
 
+    
 
+    let mut wv_edited_I = false;
+    loop {
+        //Ops. mister internett -> du må bli master (single elevator mode)
+        match main_local_chs.mpscs.rxs.udp_wv.try_recv() {
+            Ok(master_wv) => {
+                worldview_serialised = world_view_update::join_wv(worldview_serialised, master_wv);
+                wv_edited_I = true;
+            },
+            Err(_) => {}, 
+        }
+        match main_local_chs.mpscs.rxs.tcp_to_master_failed.try_recv() {
+            Ok(_) => {
+                //fikse wv
+                let mut deserialized_wv = world_view::deserialize_worldview(&worldview_serialised);
+                deserialized_wv.elevator_containers.retain(|elevator| elevator.elevator_id == utils::SELF_ID.load(Ordering::SeqCst));
+                deserialized_wv.set_num_elev(deserialized_wv.elevator_containers.len() as u8);
+                deserialized_wv.master_id = utils::SELF_ID.load(Ordering::SeqCst);
+                worldview_serialised = world_view::serialize_worldview(&deserialized_wv);
+                wv_edited_I = true;
+            },
+            Err(_) => {},
+        }
+        match main_local_chs.mpscs.rxs.container.try_recv() {
+            Ok(container) => {
+                let deser_container = world_view::deserialize_elev_container(&container);
+                let mut deserialized_wv = world_view::deserialize_worldview(&worldview_serialised);
+                if let Some(index) = deserialized_wv.elevator_containers.iter().position(|x| x.elevator_id == deser_container.elevator_id) {
+                    //TODO: sjekk at den er riktig / som forventa?
+                    deserialized_wv.elevator_containers[index] = deser_container;
+                } else {
+                    deserialized_wv.add_elev(deser_container);
+                } 
+                worldview_serialised = world_view::serialize_worldview(&deserialized_wv);
+                wv_edited_I = true;
+            },
+            Err(_) => {},
+        }
+        match main_local_chs.mpscs.rxs.remove_container.try_recv() {
+            Ok(id) => {
+                let mut deserialized_wv = world_view::deserialize_worldview(&worldview_serialised);
+                deserialized_wv.remove_elev(id);
+                worldview_serialised = world_view::serialize_worldview(&deserialized_wv);
+                wv_edited_I = true; 
+            },
+            Err(_) => {},
+        }
+        match main_local_chs.mpscs.rxs.local_elev.try_recv() {
+            Ok(msg) => {
+                let mut deserialized_wv = world_view::deserialize_worldview(&worldview_serialised);
+                let self_idx = world_view::get_index_to_container(utils::SELF_ID.load(Ordering::SeqCst) , worldview_serialised);
+                match msg.msg_type {
+                    local_network::ElevMsgType::CBTN => {
+                        print_info(format!("Callbutton: {:?}", msg.call_button));
 
+                    }
+                    local_network::ElevMsgType::FSENS => {
+                        if let (Some(i), Some(floor)) = (self_idx, msg.floor_sensor) {
+                            deserialized_wv.elevator_containers[i].last_floor_sensor = floor;
+                        }
+                        
+                    }
+                    local_network::ElevMsgType::SBTN => {
+                        print_info(format!("Stop button: {:?}", msg.stop_button));
+                        
+                    }
+                    local_network::ElevMsgType::OBSTRX => {
+                        print_info(format!("Obstruction: {:?}", msg.obstruction));
+                        if let (Some(i), Some(obs)) = (self_idx, msg.obstruction) {
+                            deserialized_wv.elevator_containers[i].obstruction = obs;
+                        }
+                    }
+                }
+                worldview_serialised = world_view::serialize_worldview(&deserialized_wv);
+                wv_edited_I = true;
+            },
+            Err(_) => {},
+        }
+        // let mut ww_des = world_view::deserialize_worldview(&worldview_serialised);
+        // ww_des.elevator_containers[0].last_floor_sensor = (ww_des.elevator_containers[0].last_floor_sensor %255) + 1;
+        // worldview_serialised = world_view::serialize_worldview(&ww_des);
+        if wv_edited_I {
+            let _ = main_local_chs.watches.txs.wv.send(worldview_serialised.clone());
+            // println!("WV er endra");
+            //println!("Worldview ble endra");
+            wv_edited_I = false;
+        }
+    }
     
     
 
