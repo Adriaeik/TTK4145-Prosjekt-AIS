235bf6277837b55cf4a6b996e6fe5d5d469f61a7
ravnbudde <159009459+ravnbudde@users.noreply.github.com>
2025-02-25 11:17:34 +0100

    Revert "nå"
    This reverts commit 41d236c38872b6bdb1b69c6bc36d80f8fa322709.

--- elevator_pro/src/utils.rs
+++ elevator_pro/src/utils.rs
@@ -188,16 +188,31 @@ pub fn get_wv(mut chs: local_network::LocalChannels) -> Vec<u8> {
 /// 
 /// Funksjonen vil endre wv til ny worldview om en ny worldview har kommet på wv_kanalen
 pub fn update_worldview(mut chs: local_network::LocalChannels, org_wv: &mut Vec<u8>) {
-    chs.resubscribe_broadcast();
     while let Ok(new_wv) = chs.broadcasts.rxs.wv.try_recv() {
         *org_wv = new_wv;
     }
 }
 
 
-pub fn is_master(mut chs: local_network::LocalChannels, mut wv: Vec<u8>) -> bool {
+pub fn is_master(mut chs: local_network::LocalChannels) -> bool {
+    let mut wv_option = None;
     chs.resubscribe_broadcast();
-    update_worldview(chs, &mut wv);
+    let mut wv: Vec<u8> = Vec::new();
+
+    while wv_option.is_none() {
+        wv_option = {
+            let mut latest_msg = None;
+            while let Ok(message) = chs.broadcasts.rxs.wv.try_recv() {
+                latest_msg = Some(message); // Overskriv tidligere meldinger
+            }
+            latest_msg
+        };
+
+        if let Some(ref msg) = wv_option {
+            wv = msg.clone();
+        } 
+    }
+
     return SELF_ID.load(Ordering::SeqCst) == wv[config::MASTER_IDX];
 }
 
