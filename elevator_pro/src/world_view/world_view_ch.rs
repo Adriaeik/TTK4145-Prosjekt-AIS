7ecce4c7f127d57787d75f334ebd09b6050cce3b
ravnbudde <159009459+ravnbudde@users.noreply.github.com>
2025-03-04 16:54:29 +0100

    print

--- elevator_pro/src/world_view/world_view_ch.rs
+++ elevator_pro/src/world_view/world_view_ch.rs
@@ -174,6 +174,7 @@ fn clear_sent_container_stuff(wv: &mut Vec<u8>, tcp_container: Vec<u8>) -> bool 
     if let Some(i) = self_idx {
         /* Oppdater task_statuser */
         deserialized_wv.elevator_containers[i].tasks_status = tcp_container_des.tasks_status;
+        println!("Oppdatert status");
         /*_____ Fjern Tasks som er markert som ferdig av slaven _____ */
         deserialized_wv.elevator_containers[i].tasks.retain(|t| !completed_tasks_ids.contains(&t.id));
         /*_____ Fjern sendte CallButtons _____ */
