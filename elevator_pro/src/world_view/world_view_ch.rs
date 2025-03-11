9f4c1f35a8cfc747ae0565f35da48fdadeb448ad
philliab13 <philliab13@ntnu.no>
2025-03-11 11:08:18 +0100

    teste switch

--- elevator_pro/src/world_view/world_view_ch.rs
+++ elevator_pro/src/world_view/world_view_ch.rs
@@ -73,86 +73,135 @@ pub async fn update_wv(mut main_local_chs: LocalChannels, mut worldview_serialis
     let _ = main_local_chs.watches.txs.wv.send(worldview_serialised.clone());
     
     let mut wv_edited_I = false;
+
     loop {
-        //OBS: Error kommer når kanal er tom. ikke print der uten å eksplisitt eksludere channel_empty error type
-
-/* KANALER SLAVE HOVEDSAKLIG MOTTAR PÅ */
-        /*_____Fjerne knappar som vart sendt på TCP_____ */
-        match main_local_chs.mpscs.rxs.sent_tcp_container.try_recv() {
-            Ok(msg) => {
+        tokio::select! {
+            /* KANALER SLAVE MOTTAR PÅ */
+            Some(msg) = main_local_chs.mpscs.rxs.sent_tcp_container.recv() => {
                 wv_edited_I = clear_from_sent_tcp(&mut worldview_serialised, msg);
-            },
-            Err(_) => {},
-        }
-        /*_____Oppdater WV fra UDP-melding_____ */
-        match main_local_chs.mpscs.rxs.udp_wv.try_recv() {
-            Ok(master_wv) => {
+            }
+            Some(master_wv) = main_local_chs.mpscs.rxs.udp_wv.recv() => {
                 wv_edited_I = join_wv_from_udp(&mut worldview_serialised, master_wv);
-            },
-            Err(_) => {}, 
-        }
-        /*_____Signal om at tilkobling til master har feila_____ */
-        match main_local_chs.mpscs.rxs.tcp_to_master_failed.try_recv() {
-            Ok(_) => {
+            }
+            Some(_) = main_local_chs.mpscs.rxs.tcp_to_master_failed.recv() => {
                 wv_edited_I = abort_network(&mut worldview_serialised);
-            },
-            Err(_) => {},
-        }
-        
-        
-/* KANALER MASTER HOVEDSAKLIG MOTTAR PÅ */
-        /*_____Melding til master fra slaven (elevator-containeren til slaven)_____*/
-        match main_local_chs.mpscs.rxs.container.try_recv() {
-            Ok(container) => {
+            }
+
+            /* KANALER MASTER MOTTAR PÅ */
+            Some(container) = main_local_chs.mpscs.rxs.container.recv() => {
                 wv_edited_I = join_wv_from_tcp_container(&mut worldview_serialised, container).await;
-            },
-            Err(_) => {},
-        }
-        /*_____ID til slave som er død (ikke kontakt med slave)_____ */
-        match main_local_chs.mpscs.rxs.remove_container.try_recv() {
-            Ok(id) => {
-                wv_edited_I = remove_container(&mut worldview_serialised, id); 
-            },
-            Err(_) => {},
-        }
-        match main_local_chs.mpscs.rxs.new_task.try_recv() {
-            Ok((task ,id, button)) => {
-                // utils::print_master(format!("Fikk task: {:?}", task));
+            }
+            Some(id) = main_local_chs.mpscs.rxs.remove_container.recv() => {
+                wv_edited_I = remove_container(&mut worldview_serialised, id);
+            }
+            Some((task, id, button)) = main_local_chs.mpscs.rxs.new_task.recv() => {
                 wv_edited_I = push_task(&mut worldview_serialised, task, id, button);
-            },
-            Err(_) => {},
-        }
-        
+            }
 
-
-/* KANALER MASTER OG SLAVE MOTTAR PÅ */
-        /*_____Knapper trykket på lokal heis_____ */
-        match main_local_chs.mpscs.rxs.local_elev.try_recv() {
-            Ok(msg) => {
+            /* KANALER MASTER OG SLAVE MOTTAR PÅ */
+            Some(msg) = main_local_chs.mpscs.rxs.local_elev.recv() => {
                 wv_edited_I = recieve_local_elevator_msg(&mut worldview_serialised, msg).await;
-            },
-            Err(_) => {},
-        }
-        /*____Får signal når en task er ferdig_____ */
-        match main_local_chs.mpscs.rxs.update_task_status.try_recv() {
-            Ok((id, status)) => {
+            }
+            Some((id, status)) = main_local_chs.mpscs.rxs.update_task_status.recv() => {
                 println!("Skal sette status {:?} på task id: {}", status, id);
                 wv_edited_I = update_task_status(&mut worldview_serialised, id, status);
-            },
-            Err(_) => {},
+            }
+
+            /* Timeout for å unngå 100% CPU-bruk */
+            _ = tokio::time::sleep(tokio::time::Duration::from_millis(1)) => {}
         }
 
-        
-
-/* KANALER ALLE SENDER LOKAL WV PÅ */
-        /*_____Hvis worldview er endra, oppdater kanalen_____ */
+        /* Hvis worldview er oppdatert, send til andre */
         if wv_edited_I {
             let _ = main_local_chs.watches.txs.wv.send(worldview_serialised.clone());
-            // println!("Sendte worldview lokalt {}", worldview_serialised[1]);
-    
             wv_edited_I = false;
         }
     }
+    
+
+
+
+
+//     loop {
+//         //OBS: Error kommer når kanal er tom. ikke print der uten å eksplisitt eksludere channel_empty error type
+
+// /* KANALER SLAVE HOVEDSAKLIG MOTTAR PÅ */
+//         /*_____Fjerne knappar som vart sendt på TCP_____ */
+//         match main_local_chs.mpscs.rxs.sent_tcp_container.try_recv() {
+//             Ok(msg) => {
+//                 wv_edited_I = clear_from_sent_tcp(&mut worldview_serialised, msg);
+//             },
+//             Err(_) => {},
+//         }
+//         /*_____Oppdater WV fra UDP-melding_____ */
+//         match main_local_chs.mpscs.rxs.udp_wv.try_recv() {
+//             Ok(master_wv) => {
+//                 wv_edited_I = join_wv_from_udp(&mut worldview_serialised, master_wv);
+//             },
+//             Err(_) => {}, 
+//         }
+//         /*_____Signal om at tilkobling til master har feila_____ */
+//         match main_local_chs.mpscs.rxs.tcp_to_master_failed.try_recv() {
+//             Ok(_) => {
+//                 wv_edited_I = abort_network(&mut worldview_serialised);
+//             },
+//             Err(_) => {},
+//         }
+        
+        
+// /* KANALER MASTER HOVEDSAKLIG MOTTAR PÅ */
+//         /*_____Melding til master fra slaven (elevator-containeren til slaven)_____*/
+//         match main_local_chs.mpscs.rxs.container.try_recv() {
+//             Ok(container) => {
+//                 wv_edited_I = join_wv_from_tcp_container(&mut worldview_serialised, container).await;
+//             },
+//             Err(_) => {},
+//         }
+//         /*_____ID til slave som er død (ikke kontakt med slave)_____ */
+//         match main_local_chs.mpscs.rxs.remove_container.try_recv() {
+//             Ok(id) => {
+//                 wv_edited_I = remove_container(&mut worldview_serialised, id); 
+//             },
+//             Err(_) => {},
+//         }
+//         match main_local_chs.mpscs.rxs.new_task.try_recv() {
+//             Ok((task ,id, button)) => {
+//                 // utils::print_master(format!("Fikk task: {:?}", task));
+//                 wv_edited_I = push_task(&mut worldview_serialised, task, id, button);
+//             },
+//             Err(_) => {},
+//         }
+        
+
+
+// /* KANALER MASTER OG SLAVE MOTTAR PÅ */
+//         /*_____Knapper trykket på lokal heis_____ */
+//         match main_local_chs.mpscs.rxs.local_elev.try_recv() {
+//             Ok(msg) => {
+//                 wv_edited_I = recieve_local_elevator_msg(&mut worldview_serialised, msg).await;
+//             },
+//             Err(_) => {},
+//         }
+//         /*____Får signal når en task er ferdig_____ */
+//         match main_local_chs.mpscs.rxs.update_task_status.try_recv() {
+//             Ok((id, status)) => {
+//                 println!("Skal sette status {:?} på task id: {}", status, id);
+//                 wv_edited_I = update_task_status(&mut worldview_serialised, id, status);
+//             },
+//             Err(_) => {},
+//         }
+
+        
+
+// /* KANALER ALLE SENDER LOKAL WV PÅ */
+//         /*_____Hvis worldview er endra, oppdater kanalen_____ */
+//         if wv_edited_I {
+//             let _ = main_local_chs.watches.txs.wv.send(worldview_serialised.clone());
+//             // println!("Sendte worldview lokalt {}", worldview_serialised[1]);
+    
+//             wv_edited_I = false;
+//         }
+//     }
 }
 
 
