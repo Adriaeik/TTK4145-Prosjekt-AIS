0efb2451bcb0b630bdcf20c8128cff7810811f94
philliab13 <philliab13@ntnu.no>
2025-03-11 22:27:56 +0100

    checkout time

--- elevator_pro/src/elevator_logic/task_handler.rs
+++ elevator_pro/src/elevator_logic/task_handler.rs
@@ -60,11 +60,13 @@ pub async fn execute_tasks(chs: local_network::LocalChannels, elevator: elev::El
                     last_state = ElevatorStatus::IDLE;
                 }
             }
-        } else {
-            elevator.motor_direction(elev::DIRN_STOP);
-            let _ = chs.mpscs.txs.update_elev_state.send(ElevatorStatus::IDLE).await;
-            last_state = ElevatorStatus::IDLE;
-            sleep(Duration::from_millis(100));
-        }
+        } 
+        
+        // else {
+        //     elevator.motor_direction(elev::DIRN_STOP);
+        //     let _ = chs.mpscs.txs.update_elev_state.send(ElevatorStatus::IDLE).await;
+        //     last_state = ElevatorStatus::IDLE;
+        //     sleep(Duration::from_millis(100));
+        // }
     }
 }
