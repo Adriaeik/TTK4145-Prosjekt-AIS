c695e1e3b992470b875ae0aecae76fb8318041fe
ravnbudde <159009459+ravnbudde@users.noreply.github.com>
2025-03-18 17:23:37 +0100

    lys

---
+++ elevator_pro_rebrand/src/elevator_logic/lights.rs
@@ -0,0 +1,56 @@
+use crate::{elevio::elev::Elevator, world_view};
+
+/// Sets all hall lights
+/// 
+/// ## Parameters
+/// `wv`: Serialized worldview
+/// `e`: Elevator instance
+/// 
+/// ## Behavior:
+/// The function goes through all hall requests in the worldview, and sets hall lights if the corresponding lights on/off based on the boolean value in the worldview.   
+/// The function skips any hall lights on floors grater than the elevators num_floors, as well as down on floor nr. 0 and up on floor nr. e.num_floors 
+/// 
+/// ## Note
+/// The function only sets the lights once per call, and needs to be recalled every time the lights needs to be updated
+pub fn set_hall_lights(wv: Vec<u8>, e: Elevator) {
+    let wv_deser = world_view::serial::deserialize_worldview(&wv);
+
+    for (i, [up, down]) in wv_deser.hall_request.iter().enumerate() {
+        let floor = i as u8;
+        if floor > e.num_floors {
+            break;
+        }
+    
+        if floor != 0 {
+            e.call_button_light(floor, 1, *down);
+        }
+        if floor != e.num_floors {
+            e.call_button_light(floor, 0, *up);
+        }
+    }
+}
+
+/// The function sets the cab light on last_floor_sensor
+pub fn set_cab_light(e: Elevator, last_floor: u8) {
+    e.floor_indicator(last_floor);
+}
+
+/// The function sets the door open light on
+pub fn set_door_open_light(e: Elevator) {
+    e.door_light(true);
+}
+
+/// The function sets the door open light off
+pub fn clear_door_open_light(e: Elevator) {
+    e.door_light(false);
+}
+
+/// The function sets the stop button light on
+pub fn set_stop_button_light(e: Elevator) {
+    e.stop_button_light(true);
+}
+
+/// The function sets the stop button light off
+pub fn clear_stop_button_light(e: Elevator) {
+    e.stop_button_light(false);
+}
