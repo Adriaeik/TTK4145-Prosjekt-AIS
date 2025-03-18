972e0bc09e593d3eca0040ef10240fdd0356ebbb
philliab13 <philliab13@ntnu.no>
2025-03-18 18:08:53 +0100

    kokt ckode

--- elevator_pro_rebrand/src/elevator_logic/fsm.rs
+++ elevator_pro_rebrand/src/elevator_logic/fsm.rs
@@ -2,83 +2,97 @@ use std::task;
 
 use crate::world_view::{Dirn, ElevatorBehaviour, ElevatorContainer};
 
-
-#[derive(Debug)]
-enum ElevatorEvent {
-    FloorArrival(usize),
-    DoorTimeout,
-    Stop,
-}
-fn make_event(elevator: &mut ElevatorContainer) -> ElevatorEvent {
-    let floor = elevator.last_floor_sensor as usize;
-
-    if elevator.tasks[floor][0] || elevator.tasks[floor][1] {
-        return ElevatorEvent::FloorArrival(floor);
-    }
-
-    let next_dir = determine_next_direction(elevator);
-    if next_dir == Dirn::Stop {
-        return ElevatorEvent::Stop;
-    }
-
-    elevator.dirn = next_dir;
-    ElevatorEvent::FloorArrival(floor + match next_dir {
-        Dirn::Up => 1,
-        Dirn::Down => -1,
-        Dirn::Stop => 0,
-    } as usize)
-}
-
-fn handle_elevator_event(elevator: &mut ElevatorContainer, tx: &Sender<ElevatorEvent>) {
-    let event = make_event(elevator);
-
-    
+#[derive(Debug, Clone, Copy)]
+struct DirnBehaviourPair {
+    dirn: Dirn,
+    behaviour: ElevatorBehaviour,
 }
 
 
-fn determine_next_direction(elevator: &ElevatorContainer) -> Dirn {
-    let current_floor = elevator.last_floor_sensor;
 
-    match elevator.dirn {
-        Dirn::Stop => {
-            // request_here
-            // Sjekk etter næraste oppdrag, prioriterer opp
-            for floor in current_floor..elevator.tasks.len() as u8 {
-                if elevator.tasks[floor as usize][0] || elevator.tasks[floor as usize][1] {
-                    return if floor > current_floor { Dirn::Up } else { Dirn::Down };
-                }
+/////Requests
+fn requests_above(elevator: &ElevatorContainer) -> bool {
+    for floor in (elevator.last_floor_sensor as usize + 1)..elevator.tasks.len() {
+        for btn in 0..2 {
+            if elevator.tasks[floor][btn] {
+                return true;
             }
-            Dirn::Stop
         }
+    }
+    false
+}
+
+fn requests_below(elevator: &ElevatorContainer) -> bool {
+    for floor in 0..elevator.last_floor_sensor as usize {
+        for btn in 0..2 {
+            if elevator.tasks[floor][btn] {
+                return true;
+            }
+        }
+    }
+    false
+}
+
+fn requests_here(elevator: &ElevatorContainer) -> bool {
+    for btn in 0..2 {
+        if elevator.tasks[elevator.last_floor_sensor as usize][btn] {
+            return true;
+        }
+    }
+    false
+}
+
+fn requests_choose_direction(elevator: &ElevatorContainer) -> DirnBehaviourPair {
+    match elevator.dirn {
         Dirn::Up => {
-            // Fortsett oppover dersom det finst fleire oppdrag
-            for floor in (current_floor + 1)..elevator.tasks.len() as u8 {
-                if elevator.tasks[floor as usize][0] {
-                    return Dirn::Up;
-                }
+            if requests_above(elevator) {
+                DirnBehaviourPair { dirn: Dirn::Up, behaviour: ElevatorBehaviour::Moving }
+            } else if requests_here(elevator) {
+                DirnBehaviourPair { dirn: Dirn::Down, behaviour: ElevatorBehaviour::DoorOpen }
+            } else if requests_below(elevator) {
+                DirnBehaviourPair { dirn: Dirn::Down, behaviour: ElevatorBehaviour::Moving }
+            } else {
+                DirnBehaviourPair { dirn: Dirn::Stop, behaviour: ElevatorBehaviour::Idle }
             }
-            // Dersom ingen fleire oppdrag opp, sjekk nedover
-            for floor in (0..=current_floor).rev() {
-                if elevator.tasks[floor as usize][1] {
-                    return Dirn::Down;
-                }
-            }
-            Dirn::Stop
         }
         Dirn::Down => {
-            // Fortsett nedover dersom det finst fleire oppdrag
-            for floor in (0..=current_floor).rev() {
-                if elevator.tasks[floor as usize][1] {
-                    return Dirn::Down;
-                }
+            if requests_below(elevator) {
+                DirnBehaviourPair { dirn: Dirn::Down, behaviour: ElevatorBehaviour::Moving }
+            } else if requests_here(elevator) {
+                DirnBehaviourPair { dirn: Dirn::Up, behaviour: ElevatorBehaviour::DoorOpen }
+            } else if requests_above(elevator) {
+                DirnBehaviourPair { dirn: Dirn::Up, behaviour: ElevatorBehaviour::Moving }
+            } else {
+                DirnBehaviourPair { dirn: Dirn::Stop, behaviour: ElevatorBehaviour::Idle }
             }
-            // Dersom ingen fleire oppdrag ned, sjekk oppover
-            for floor in current_floor..elevator.tasks.len() as u8 {
-                if elevator.tasks[floor as usize][0] {
-                    return Dirn::Up;
-                }
+        }
+        Dirn::Stop => {
+            if requests_here(elevator) {
+                DirnBehaviourPair { dirn: Dirn::Stop, behaviour: ElevatorBehaviour::DoorOpen }
+            } else if requests_above(elevator) {
+                DirnBehaviourPair { dirn: Dirn::Up, behaviour: ElevatorBehaviour::Moving }
+            } else if requests_below(elevator) {
+                DirnBehaviourPair { dirn: Dirn::Down, behaviour: ElevatorBehaviour::Moving }
+            } else {
+                DirnBehaviourPair { dirn: Dirn::Stop, behaviour: ElevatorBehaviour::Idle }
             }
-            Dirn::Stop
         }
     }
+}
+
+fn requests_should_stop(elevator: &ElevatorContainer) -> bool {
+    match elevator.dirn {
+        Dirn::Down => {
+            elevator.tasks[elevator.last_floor_sensor as usize][1] || !requests_below(elevator)
+        }
+        Dirn::Up => {
+            elevator.tasks[elevator.last_floor_sensor as usize][0] || !requests_above(elevator)
+        }
+        Dirn::Stop => true,
+    }
+}
+
+fn requests_clear_at_current_floor(mut elevator: ElevatorContainer) -> ElevatorContainer {
+    elevator.tasks[elevator.last_floor_sensor as usize] = [false, false];
+    elevator
 }
