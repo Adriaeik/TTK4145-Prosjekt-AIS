eb2c8163950efc39d6283492c8bc9b590d9e95be
adriaeik <adriaeik@stud.ntnu.no>
2025-03-22 15:27:27 +0100

    build

--- elevator_pro_rebrand/libs/Project_resources/cost_fns/hall_request_assigner/build.sh
+++ elevator_pro_rebrand/libs/Project_resources/cost_fns/hall_request_assigner/build.sh
@@ -1,13 +1,13 @@
-# #!/bin/bash
+#!/bin/bash
 
-# # Stoppar skriptet med ein gong viss noko feilar
-# set -ex
+# Stoppar skriptet med ein gong viss noko feilar
+set -ex
 
-# # Sørg for at vi har rettar til å køyre kompilert binær
-# sudo chmod +x d-json/jsonx.d || true
+# Sørg for at vi har rettar til å køyre kompilert binær
+sudo chmod +x d-json/jsonx.d || true
 
-# # Kompilerer prosjektet
+# Kompilerer prosjektet
 dmd main.d config.d elevator_algorithm.d elevator_state.d optimal_hall_requests.d d-json/jsonx.d -w -g -ofhall_request_assigner
 
-# # Sørg for at kompilert fil kan køyrast
-# sudo chmod +x hall_request_assigner
+# Sørg for at kompilert fil kan køyrast
+sudo chmod +x hall_request_assigner
